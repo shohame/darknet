1e2943a33ccf18d67423378233853352f5b15c25
Perry Shoham <perry.shoham@gmail.com>
2020-04-11 12:33:13 +0300

    Release and Debug compiled on VS 2019 with GPU. Next step: To run Debug on CPU!

--- build/darknet/x64/darknet_voc.cmd
+++ build/darknet/x64/darknet_voc.cmd
@@ -2,7 +2,7 @@
 rem darknet.exe detector test cfg/voc.data cfg/yolov2-voc.cfg yolo-voc.weights 009460.jpg
 
 
-darknet.exe detector test cfg/voc.data cfg/yolov2-voc.cfg yolo-voc.weights -i 0 -thresh 0.24 dog.jpg
+darknet.exe detector test cfg/voc.data cfg/yolov2-voc.cfg yolo2-voc.weights -i 0 -thresh 0.24 dog.jpg
 
 
 pause
